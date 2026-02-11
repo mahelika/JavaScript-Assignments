f0c83d98b1b714fb185ca0699ab9d3b5d1a75935
mahelika <imahelika@gmail.com>
2026-02-12 02:38:15 +0530

    js-built-ins done

--- js-built-ins/problems/easy/fibonacci.js
+++ js-built-ins/problems/easy/fibonacci.js
@@ -40,11 +40,24 @@
 // Iterative Fibonacci function
 function fibonacci(n) {
   // Your code here
+  if (n===0) return [];
+  if (n===1) return [0];
+
+  const result = [0,1];
+
+  for (let i=2; i<n; i++){
+    result.push(result[i-1]+ result[i-2]);
+  }
+
+  return result;
 }
 
 // Recursive Fibonacci function
 function fibonacciRecursive(n) {
   // Your code here
+  if(n<=1) return n;
+
+  return fibonacciRecursive(n-1)+fibonacciRecursive(n-2);
 }
 
 module.exports = { fibonacci, fibonacciRecursive };
--- js-built-ins/problems/easy/reverseString.js
+++ js-built-ins/problems/easy/reverseString.js
@@ -20,6 +20,13 @@
 
 function reverseString(str) {
   // Your code here
+
+  let reversed = "";
+  for (let i=str.length-1; i>=0; i--){
+    reversed += str[i];
+  }
+
+  return reversed;
 }
 
 module.exports = reverseString;
